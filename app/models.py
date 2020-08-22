673ca838781dfaffd82946ba62d010d8c81839af
jeffreylozano1376 <jeffreylozano1376@yahoo.com.ph>
2020-08-22 17:58:36 +0800

    dfsa

--- app/models.py
+++ app/models.py
@@ -6,16 +6,20 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    service_items = db.relationship('Service_Item', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
 
-class Post(db.Model):
+class Service_Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    customer = db.Column(db.String(64), index=True)
+    item_brand = db.Column(db.String(64), index=True)
+    item_model = db.Column(db.String(64), index=True)
+    serial_num = db.Column(db.Integer, index=True)
+    req_serv = db.Column(db.String(64), index=True)
+    date_recvd = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f'<Post {self.body}>'
+        return f'<Service_Item {self.customer} {self.item_brand} {self.item_model}>'
